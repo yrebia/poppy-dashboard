cc57bc80e1f174a6a4e9b7516d3e537658d13c61
yrebia <yanis.rebia@epitech.eu>
2024-07-01 10:19:31 +0200

    Adding netlify config

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 
 mongoose
-    .connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGODB_URL)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("Failed to connect to MongoDB",))
